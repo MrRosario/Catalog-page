a292f9d3c23d0e5222a16d4d789b94cae17d84c5
MrRosario <eugenio.dorosario94@gmail.com>
2023-12-13 17:26:15 -0300

    fix bug when fetching categories

--- src/hook/useFetch.ts
+++ src/hook/useFetch.ts
@@ -1,26 +1,18 @@
 import { useEffect, useState } from "react";
-import { fetcher } from "@/utils/helpers";
 
 export const useFetch = (url: string) => {
-  const [data, setData] = useState<any>([]);
+  const [data, setData] = useState<any>(null);
   const [error, setError] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const fetchData = async () => {
-    setLoading(true);
-    try {
-      const response = await fetcher(url);
-      setData(response);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   useEffect(() => {
-    fetchData();
-  }, []);
+    setLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, [url]);
 
-  return { data, error, loading, fetchData };
+  return { data, error, loading };
 };
